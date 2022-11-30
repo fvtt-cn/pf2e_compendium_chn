53945f7d78836a6801a6ecc2fee8c700de96befb
Kuroni <90250769+AlphaStarguide@users.noreply.github.com>
2022-11-30 12:02:15 +0900

    Add files via upload

--- babele.js
+++ babele.js
@@ -1,9 +1,24 @@
-Hooks.on('init', () => {
-  if (typeof Babele !== 'undefined') {
+Hooks.once("init", () => {
+  if (typeof Babele !== "undefined") {
     Babele.get().register({
-      module: 'pf2e_compendium_chn',
-      lang: 'cn',
-      dir: 'compendium'
+      module: "pf2e_compendium_chn",
+      lang: "cn",
+      dir: "compendium",
+    });
+
+    Babele.get().registerConverters({
+      "npc-portrait-path": (data, translations, dataObject, translatedCompendium, translationObject) => {
+        return game.npcTrans.portrait(data, translations, dataObject, translatedCompendium, translationObject);
+      },
+      "npc-token-translation": (data, translations, dataObject, translatedCompendium, translationObject) => {
+        return game.npcTrans.token(data, translations, dataObject, translatedCompendium, translationObject);
+      },
+      "npc-data-translation": (data, translations, dataObject, translatedCompendium, translationObject) => {
+        return game.npcTrans.data(data, translations, dataObject, translatedCompendium, translationObject);
+      },
+      "npc-item-translation": (data, translations, dataObject, translatedCompendium, translationObject) => {
+        return game.npcTrans.item(data, translations, dataObject, translatedCompendium, translationObject);
+      },
     });
   }
 });
